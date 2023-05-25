ed819b3d06233e2312e060984701cec7f022fded
bujosa <davidbujosa@gmail.com>
2023-05-25 18:11:49 -0400

    feat: add three sum

--- README.md
+++ README.md
@@ -10,5 +10,7 @@ In this project, I will try to solve leetcode challenges in rust.
 - [ ] [Easy](src/easy)
   - [x] [13. Roman to interger](src/easy/roman_to_integer.rs)
   - [x] [14. Longest common prefix](src/easy/longest_common_prefix.rs)
+  - [x] [15. Three sum](src/easy/three_sum.rs)
+  
 - [ ] [Medium](src/medium)
 - [ ] [Hard](src/hard)
---
+++ src/easy/three_sum.rs
@@ -0,0 +1,46 @@
+pub fn three_sum(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    let mut nums = nums;
+    nums.sort();
+    let mut res = vec![];
+    for i in 0..nums.len() {
+        if i > 0 && nums[i] == nums[i - 1] {
+            continue;
+        }
+        let mut l = i + 1;
+        let mut r = nums.len() - 1;
+        while l < r {
+            let sum = nums[i] + nums[l] + nums[r];
+            if sum == 0 {
+                res.push(vec![nums[i], nums[l], nums[r]]);
+                while l < r && nums[l] == nums[l + 1] {
+                    l += 1;
+                }
+                while l < r && nums[r] == nums[r - 1] {
+                    r -= 1;
+                }
+                l += 1;
+                r -= 1;
+            } else if sum < 0 {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_15() {
+        assert_eq!(
+            three_sum(vec![-1, 0, 1, 2, -1, -4]),
+            vec![vec![-1, -1, 2], vec![-1, 0, 1]]
+        );
+    }
+}
+
+// Reference: https://leetcode.com/problems/3sum/
