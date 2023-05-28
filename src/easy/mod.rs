75170e2f5fd465468a208c638efcb9816b7aa718
bujosa <davidbujosa@gmail.com>
2023-05-27 21:12:29 -0400

    feat: add palindrome linked list

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,3 +1,4 @@
 pub mod roman_to_integer;
 pub mod longest_common_prefix;
 pub mod palindrome_number;
+pub mod palindrome_linked_list;
---
+++ src/easy/palindrome_linked_list.rs
@@ -0,0 +1,51 @@
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>
+}
+
+pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
+    let mut v = Vec::new();
+    let mut current = head;
+    while let Some(node) = current {
+        v.push(node.val);
+        current = node.next;
+    }
+    let mut i = 0;
+    let mut j = v.len() - 1;
+    while i < j {
+        if v[i] != v[j] {
+            return false;
+        }
+        i += 1;
+        j -= 1;
+    }
+    true
+}
+
+#[cfg(test)]
+fn create_list_node(v: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut head = None;
+    for i in v.iter().rev() {
+        let mut node = ListNode {
+            val: *i,
+            next: None
+        };
+        node.next = head;
+        head = Some(Box::new(node));
+    }
+    head
+}
+
+#[test]
+fn test_is_palindrome() {
+    let v = vec![1, 2];
+    let head = create_list_node(v);
+    let result = is_palindrome(head);
+    assert_eq!(result, false);
+
+    let v = vec![1, 2, 2, 1];
+    let head = create_list_node(v);
+    let result = is_palindrome(head);
+    assert_eq!(result, true);
+}
