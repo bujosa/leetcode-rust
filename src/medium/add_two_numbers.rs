986b8e2097877d49e9300dc9189561590da5b2c8
bujosa <davidbujosa@gmail.com>
2023-05-30 01:11:20 -0400

    refactor: make more readable "add two numbers"

--- src/medium/add_two_numbers.rs
+++ src/medium/add_two_numbers.rs
@@ -17,18 +17,24 @@ pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> 
     let mut head = Some(Box::new(ListNode::new(0)));
     let mut tail = &mut head;
     let mut carry = 0;
+
     while l1.is_some() || l2.is_some() || carry != 0 {
         let mut sum = carry;
+
         if let Some(node) = l1 {
             sum += node.val;
             l1 = node.next;
         }
+
         if let Some(node) = l2 {
             sum += node.val;
             l2 = node.next;
         }
+
         carry = sum / 10;
+
         tail.as_mut().unwrap().next = Some(Box::new(ListNode::new(sum % 10)));
+        
         tail = &mut tail.as_mut().unwrap().next;
     }
     head.unwrap().next
