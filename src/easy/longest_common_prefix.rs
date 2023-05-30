eb2572098086574cfdff421415043e1f5f4350b0
bujosa <davidbujosa@gmail.com>
2023-05-30 01:11:34 -0400

    refactor: make more readable "longest common prefix"

--- src/easy/longest_common_prefix.rs
+++ src/easy/longest_common_prefix.rs
@@ -3,14 +3,17 @@ pub fn longest_common_prefix(strs: Vec<String>) -> String {
         return "".to_string();
     }
     let mut prefix = strs[0].clone();
+    
     for i in 1..strs.len() {
         let mut j = 0;
+
         while j < prefix.len() && j < strs[i].len() {
             if prefix.chars().nth(j).unwrap() != strs[i].chars().nth(j).unwrap() {
                 break;
             }
             j += 1;
         }
+
         prefix = prefix[..j].to_string();
     }
     prefix
