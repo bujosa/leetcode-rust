e3ac905f125e096a975b29a7c88200017817fe6e
bujosa <davidbujosa@gmail.com>
2023-05-30 01:11:42 -0400

    refactor: make more readable "two sum"

--- src/easy/two_sum.rs
+++ src/easy/two_sum.rs
@@ -2,13 +2,17 @@ use std::collections::HashMap;
 
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::new();
+
     for (i, num) in nums.iter().enumerate() {
         let complement = target - num;
+
         if map.contains_key(&complement) {
             return vec![*map.get(&complement).unwrap() as i32, i as i32];
         }
+        
         map.insert(num, i);
     }
+
     vec![]
 }
 
