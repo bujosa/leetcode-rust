ceaa79dacd8317bfea0e6869a4c66c3e934446e7
bujosa <davidbujosa@gmail.com>
2023-05-31 22:48:19 -0400

    feat: init remove element

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -1,7 +1,7 @@
 pub mod longest_common_prefix;
 pub mod merged_two_sorted_lists;
 pub mod palindrome_linked_list;
-
 pub mod palindrome_number;
+pub mod remove_element;
 pub mod roman_to_integer;
 pub mod two_sum;
