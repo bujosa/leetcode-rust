c09d1d30440b8c4116e25058cdce4a31d37279ab
bujosa <davidbujosa@gmail.com>
2023-06-01 00:29:56 -0400

    feat: add two validations in search insert

--- src/easy/search_insert_position.rs
+++ src/easy/search_insert_position.rs
@@ -5,6 +5,12 @@ pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
     let mut right = nums.len() - 1;
     let mut mid = 0;
 
+    if target < nums[0] {
+        return 0;
+    } else if target > nums[nums.len() - 1] {
+        return nums.len() as i32;
+    }
+
     while left <= right {
         mid = (left + right) / 2;
         if nums[mid] == target {
