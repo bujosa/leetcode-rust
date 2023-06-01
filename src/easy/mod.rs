3c52022f3bee45b25744339e10488c75f1f4ea8c
bujosa <davidbujosa@gmail.com>
2023-06-01 00:17:28 -0400

    feat: init search insert position

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -5,4 +5,5 @@ pub mod palindrome_linked_list;
 pub mod palindrome_number;
 pub mod remove_element;
 pub mod roman_to_integer;
+pub mod search_insert_position;
 pub mod two_sum;
---
+++ src/easy/search_insert_position.rs
@@ -0,0 +1,4 @@
+#[allow(dead_code)]
+pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        
+}
