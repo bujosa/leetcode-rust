e332510a0595a5df9b137f6d730bb765ea0e593b
bujosa <davidbujosa@gmail.com>
2023-06-01 00:30:41 -0400

    test: add validations for 0 target in search insert position

--- src/easy/search_insert_position.rs
+++ src/easy/search_insert_position.rs
@@ -45,6 +45,7 @@ fn test_search_insert() {
     assert_eq!(search_insert(vec![1, 3, 5, 6], 2), 1);
     assert_eq!(search_insert(vec![1, 3, 5, 6], 7), 4);
     assert_eq!(search_insert(vec![1, 3, 5, 6], 0), 0);
+    assert_eq!(search_insert(vec![1, 3], 0), 0);
     assert_eq!(search_insert(vec![1], 0), 0);
     assert_eq!(search_insert(vec![1], 2), 1);
 }
