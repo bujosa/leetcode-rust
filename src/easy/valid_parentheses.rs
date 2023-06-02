65865d74df461055e897ff1ba59178c8a92439f5
bujosa <davidbujosa@gmail.com>
2023-06-02 03:36:20 -0400

    feat: add valid parentheses

---
+++ src/easy/valid_parentheses.rs
@@ -0,0 +1,39 @@
+#[allow(dead_code)]
+pub fn is_valid(s: String) -> bool {
+    let mut stack = Vec::new();
+    for c in s.chars() {
+        match c {
+            '(' | '[' | '{' => stack.push(c),
+            ')' => {
+                if stack.pop() != Some('(') {
+                    return false;
+                }
+            }
+            ']' => {
+                if stack.pop() != Some('[') {
+                    return false;
+                }
+            }
+            '}' => {
+                if stack.pop() != Some('{') {
+                    return false;
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+    stack.is_empty()
+}
+
+/*
+    
+ */
+
+#[test]
+fn test_is_valid() {
+    assert_eq!(is_valid("()".to_string()), true);
+    assert_eq!(is_valid("()[]{}".to_string()), true);
+    assert_eq!(is_valid("(]".to_string()), false);
+    assert_eq!(is_valid("([)]".to_string()), false);
+    assert_eq!(is_valid("{[]}".to_string()), true);
+}
