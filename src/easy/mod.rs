848fb1f26a0878138db2c23f8cf400a9914f8219
bujosa <davidbujosa@gmail.com>
2023-06-02 19:56:07 -0400

    feat: init plus one

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -4,6 +4,7 @@ pub mod longest_common_prefix;
 pub mod merged_two_sorted_lists;
 pub mod palindrome_linked_list;
 pub mod palindrome_number;
+pub mod plus_one;
 pub mod remove_duplicates_from_sorted_array;
 pub mod remove_element;
 pub mod roman_to_integer;
