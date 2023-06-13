dc1aa57c1ef5b4516384c024f5afc59e010e1b49
bujosa <davidbujosa@gmail.com>
2023-06-13 19:56:32 -0400

    refactor: reduce extra hashmap in valid anagram

--- src/easy/valid_anagram.rs
+++ src/easy/valid_anagram.rs
@@ -2,29 +2,32 @@
 pub fn is_anagram(s: String, t: String) -> bool {
     use std::collections::HashMap;
 
-    let mut s_map: HashMap<char, i32> = HashMap::new();
-    let mut t_map: HashMap<char, i32> = HashMap::new();
+    let mut map: HashMap<char, i32> = HashMap::new();
 
     for c in s.chars() {
-        let count = s_map.entry(c).or_insert(0);
+        let count = map.entry(c).or_insert(0);
         *count += 1;
     }
 
     for c in t.chars() {
-        let count = t_map.entry(c).or_insert(0);
-        *count += 1;
+        let count = map.entry(c).or_insert(0);
+        *count -= 1;
     }
 
-    s_map == t_map
+    map.values().all(|&x| x == 0)
 }
 
 /*
   Algorithm - Hashmap
-   - Use a hashmap to store the frequency of each character in the first string
-   - Iterate through the second string and decrement the frequency of each character
-   - If the frequency of a character is 0, remove it from the hashmap
-   - If the hashmap is empty, then the strings are anagrams
-   - This solution is O(n) time and O(n) space
+   - Create a hashmap
+    - Iterate through the first string
+         - Insert each character into the hashmap with a value of 1
+    - Iterate through the second string
+           - Insert each character into the hashmap with a value of -1
+    - Iterate through the hashmap values
+        - If any value is not 0
+            - Return false
+    - Return true
 
   Time Complexity - O(n)
   Space Complexity - O(n)
