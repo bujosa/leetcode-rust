7f78e2c4538ad72ad4b8744c7d95ce8e5e5ecca9
bujosa <davidbujosa@gmail.com>
2023-06-18 02:06:53 -0400

    feat: add 150 problem solution

--- src/medium/evaluate_reverse_polish_notation.rs
+++ src/medium/evaluate_reverse_polish_notation.rs
@@ -1,7 +1,83 @@
 #![allow(dead_code)]
 pub fn eval_rpn(tokens: Vec<String>) -> i32 {
-    todo!()
+    let mut stack = Vec::new();
+
+    for token in tokens {
+        match token.as_str() {
+            "+" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(a + b);
+            }
+            "-" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b - a);
+            }
+            "*" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(a * b);
+            }
+            "/" => {
+                let a = stack.pop().unwrap();
+                let b = stack.pop().unwrap();
+                stack.push(b / a);
+            }
+            _ => {
+                stack.push(token.parse::<i32>().unwrap());
+            }
+        }
+    }
+
+    stack.pop().unwrap()
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_150() {
+        assert_eq!(
+            eval_rpn(vec![
+                "2".to_string(),
+                "1".to_string(),
+                "+".to_string(),
+                "3".to_string(),
+                "*".to_string()
+            ]),
+            9
+        );
+
+        assert_eq!(
+            eval_rpn(vec![
+                "4".to_string(),
+                "13".to_string(),
+                "5".to_string(),
+                "/".to_string(),
+                "+".to_string()
+            ]),
+            6
+        );
+
+        assert_eq!(
+            eval_rpn(vec![
+                "10".to_string(),
+                "6".to_string(),
+                "9".to_string(),
+                "3".to_string(),
+                "+".to_string(),
+                "-11".to_string(),
+                "*".to_string(),
+                "/".to_string(),
+                "*".to_string(),
+                "17".to_string(),
+                "+".to_string(),
+                "5".to_string(),
+                "+".to_string()
+            ]),
+            22
+        );
+    }
+}
