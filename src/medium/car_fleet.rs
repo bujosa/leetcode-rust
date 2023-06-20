8d58f6fdb2194ece54c4a425233e539daaca8ac5
bujosa <davidbujosa@gmail.com>
2023-06-20 11:25:08 -0400

    feat: add car fleet problem solution

--- src/medium/car_fleet.rs
+++ src/medium/car_fleet.rs
@@ -1,2 +1,28 @@
 #![allow(dead_code)]
-pub fn car_fleet(target: i32, position: Vec<i32>, speed: Vec<i32>) -> i32 {}
+pub fn car_fleet(target: i32, position: Vec<i32>, speed: Vec<i32>) -> i32 {
+    let mut cars: Vec<(i32, f64)> = position
+        .into_iter()
+        .zip(speed.into_iter().map(|x| x as f64))
+        .collect();
+
+    cars.sort_by(|a, b| b.0.cmp(&a.0));
+
+    let mut stack = vec![];
+
+    for (pos, speed) in cars {
+        stack.push((target - pos) as f64 / speed);
+
+        if stack.len() >= 2 && stack[stack.len() - 1] <= stack[stack.len() - 2] {
+            stack.pop();
+        }
+    }
+
+    stack.len() as i32
+}
+
+#[test]
+fn test_car_fleet() {
+    assert_eq!(car_fleet(12, vec![10, 8, 0, 5, 3], vec![2, 4, 1, 1, 3]), 3);
+
+    assert_eq!(car_fleet(10, vec![0, 4, 2], vec![2, 1, 3]), 1);
+}
