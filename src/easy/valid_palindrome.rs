5164fafb7eb163fad4fca5e3b58aa6c119d39145
bujosa <davidbujosa@gmail.com>
2023-06-22 19:04:08 -0400

    feat: add problem solution for 125

--- src/easy/valid_palindrome.rs
+++ src/easy/valid_palindrome.rs
@@ -1,5 +1,13 @@
 #![allow(dead_code)]
-pub fn is_palindrome(s: String) -> bool {}
+pub fn is_palindrome(s: String) -> bool {
+    let mut chars = s.chars().filter(|c| c.is_ascii_alphanumeric());
+    while let (Some(left), Some(right)) = (chars.next(), chars.next_back()) {
+        if !left.eq_ignore_ascii_case(&right) {
+            return false;
+        }
+    }
+    true
+}
 
 #[cfg(test)]
 mod tests {
