dac390df421a6180c1b1453afe94589da05b5f59
bujosa <davidbujosa@gmail.com>
2023-06-22 18:22:45 -0400

    feat: add largest rectangle in histogram solution

--- src/hard/largest_rectangle_in_histogram.rs
+++ src/hard/largest_rectangle_in_histogram.rs
@@ -1,5 +1,38 @@
 #![allow(dead_code)]
-pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {}
+pub fn largest_rectangle_area(heights: Vec<i32>) -> i32 {
+    let mut stack = vec![];
+    let mut max_area = 0;
+    let mut i = 0;
+
+    while i < heights.len() {
+        if stack.is_empty() || heights[*stack.last().unwrap()] <= heights[i] {
+            stack.push(i);
+            i += 1;
+        } else {
+            let top = stack.pop().unwrap();
+            let area = heights[top]
+                * if stack.is_empty() {
+                    i
+                } else {
+                    i - stack.last().unwrap() - 1
+                } as i32;
+            max_area = max_area.max(area);
+        }
+    }
+
+    while !stack.is_empty() {
+        let top = stack.pop().unwrap();
+        let area = heights[top]
+            * if stack.is_empty() {
+                i
+            } else {
+                i - stack.last().unwrap() - 1
+            } as i32;
+        max_area = max_area.max(area);
+    }
+
+    max_area
+}
 
 #[cfg(test)]
 mod tests {
