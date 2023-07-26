b8ea38b100a99d93dcbdd0ffcaf6c05c42d53b84
bujosa <davidbujosa@gmail.com>
2023-07-26 12:06:46 -0400

    feat: add max area function

--- src/medium/container_with_most_water.rs
+++ src/medium/container_with_most_water.rs
@@ -1,7 +1,22 @@
 #![allow(dead_code)]
 
 pub fn max_area(height: Vec<i32>) -> i32 {
-    todo!()
+    let mut max_area = 0;
+    let mut left = 0;
+    let mut right = height.len() - 1;
+
+    while left < right {
+        let area = (right - left) as i32 * std::cmp::min(height[left], height[right]);
+        max_area = std::cmp::max(max_area, area);
+
+        if height[left] < height[right] {
+            left += 1;
+        } else {
+            right -= 1;
+        }
+    }
+
+    max_area
 }
 
 #[cfg(test)]
