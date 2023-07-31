59e379e45b4f91405cd0ddef8cc43aa278848469
bujosa <davidbujosa@gmail.com>
2023-07-31 19:35:40 -0400

    feat: add solution for longest substring without repeating characters

--- src/medium/longest_substring_without_repeating_characters.rs
+++ src/medium/longest_substring_without_repeating_characters.rs
@@ -1,7 +1,39 @@
 #![allow(dead_code)]
 pub fn length_of_longest_substring(s: String) -> i32 {
-    return 0;
+    let mut max_length = 0;
+    let mut start = 0;
+
+    let mut map = std::collections::HashMap::new();
+
+    for (end, c) in s.chars().enumerate() {
+        if let Some(i) = map.get(&c) {
+            start = std::cmp::max(start, *i + 1);
+        }
+
+        map.insert(c, end);
+
+        max_length = std::cmp::max(max_length, end - start + 1);
+    }
+
+    max_length as i32
 }
+
+/*
+    Algorithm - Sliding Window
+
+    Time Complexity = O(n)
+    Space Complexity = O(n)
+
+    Explanation:
+    1. Create a HashMap to store the characters and their indices
+    2. Iterate through the string
+    3. If the character is already in the HashMap, set the start to the maximum of the current start and the index of the character + 1
+    4. Insert the character and its index into the HashMap
+    Note: The HashMap stores the index of the character + 1 because if the character is already in the HashMap, the start should be set to the index of the character + 1
+    5. Set the max_length to the maximum of the current max_length and the end - start + 1
+    6. Return the max_length
+
+*/
 
 #[cfg(test)]
 mod tests {
