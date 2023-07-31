0127e5b97b14ed65934f857562e6ffc7c5180dbf
bujosa <davidbujosa@gmail.com>
2023-07-31 19:17:26 -0400

    feat: add algorithm description for best time to buy and sell stock

--- src/easy/best_time_to_buy_and_sell_stock.rs
+++ src/easy/best_time_to_buy_and_sell_stock.rs
@@ -14,6 +14,19 @@ pub fn max_profit(prices: Vec<i32>) -> i32 {
     max_profit
 }
 
+/*
+    Algorithm - Kadane's Algorithm
+
+    Time Complexity = O(n)
+    Space Complexity = O(1)
+
+    Explanation:
+    1. Iterate through the prices vector
+    2. If the current price is less than the minimum price, set the minimum price to the current price
+    3. Else if the current price - minimum price is greater than the maximum profit, set the maximum profit to the current price - minimum price
+    4. Return the maximum profit
+*/
+
 #[cfg(test)]
 mod tests {
     use super::*;
