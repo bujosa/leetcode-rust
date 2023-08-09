60ba4fa8aad1ab9953f60a2a163e06050c181f4b
bujosa <davidbujosa@gmail.com>
2023-08-08 21:00:23 -0400

    feat: implement longest repeating character replacement

--- src/medium/longest_repeating_character_replacement.rs
+++ src/medium/longest_repeating_character_replacement.rs
@@ -1,6 +1,25 @@
 #![allow(dead_code)]
 pub fn character_replacement(s: String, k: i32) -> i32 {
-    return 0;
+    let mut max_count = 0;
+    let mut start = 0;
+    let mut max_length = 0;
+    let mut map = [0; 26];
+
+    for end in 0..s.len() {
+        let c = s.chars().nth(end).unwrap();
+        map[c as usize - 'A' as usize] += 1;
+        max_count = max_count.max(map[c as usize - 'A' as usize]);
+
+        while end - start + 1 - max_count > k as usize {
+            let c = s.chars().nth(start).unwrap();
+            map[c as usize - 'A' as usize] -= 1;
+            start += 1;
+        }
+
+        max_length = max_length.max(end - start + 1);
+    }
+
+    max_length as i32
 }
 
 #[cfg(test)]
