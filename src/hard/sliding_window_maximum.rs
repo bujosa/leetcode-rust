7d06b8c54125b24db0e7784301bc0a7540e89225
bujosa <davidbujosa@gmail.com>
2023-08-20 19:29:07 -0400

    feat: add solution for sliding window maximum

--- src/hard/sliding_window_maximum.rs
+++ src/hard/sliding_window_maximum.rs
@@ -1,5 +1,30 @@
 #![allow(dead_code)]
-pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {}
+pub fn max_sliding_window(nums: Vec<i32>, k: i32) -> Vec<i32> {
+    let mut result = Vec::new();
+    let mut window = Vec::new();
+
+    for i in 0..nums.len() {
+        // Remove the first element if it is out of the window
+        if i >= k as usize && window[0] <= i as i32 - k {
+            window.remove(0);
+        }
+
+        // Remove all elements smaller than the current one
+        while window.len() > 0 && nums[*window.last().unwrap() as usize] <= nums[i] {
+            window.pop();
+        }
+
+        // Add the current element
+        window.push(i as i32);
+
+        // Add the maximum to the result
+        if i >= k as usize - 1 {
+            result.push(nums[*window.first().unwrap() as usize]);
+        }
+    }
+
+    result
+}
 
 #[cfg(test)]
 mod tests {
