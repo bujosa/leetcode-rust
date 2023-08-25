4ff4d1988117ece92f51cdc7df4a60fe83c61551
bujosa <davidbujosa@gmail.com>
2023-08-24 21:29:30 -0400

    feat: generate doc test for reverse linked list

--- src/easy/reverse_linked_list.rs
+++ src/easy/reverse_linked_list.rs
@@ -6,12 +6,38 @@ pub struct ListNode {
 }
 
 impl ListNode {
-    #[inline]
-    fn new(val: i32) -> Self {
-        ListNode { next: None, val }
+    pub fn new(val: i32) -> Self {
+        ListNode { val, next: None }
     }
 }
 
+/// Reverses a singly linked list.
+///
+/// # Arguments
+///
+/// * `head` - A singly linked list represented as an `Option<Box<ListNode>>`.
+///
+/// # Example
+///
+/// ```
+/// use leetcode::easy::reverse_linked_list::{reverse_list, ListNode};
+///
+/// let mut node1 = ListNode::new(1);
+/// let mut node2 = ListNode::new(2);
+/// let mut node3 = ListNode::new(3);
+/// let mut node4 = ListNode::new(4);
+/// let mut node5 = ListNode::new(5);
+///
+/// node4.next = Some(Box::new(node5));
+/// node3.next = Some(Box::new(node4));
+/// node2.next = Some(Box::new(node3));
+/// node1.next = Some(Box::new(node2));
+///
+/// let reversed_list = reverse_list(Some(Box::new(node1)));
+///
+/// assert_eq!(reversed_list.as_ref().unwrap().val, 5);
+/// assert_eq!(reversed_list.as_ref().unwrap().next.as_ref().unwrap().val, 4);
+/// ```
 pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
     let mut prev = None;
     let mut curr = head;
