512b2412931c2f4728affc5a115d065823b6e259
bujosa <davidbujosa@gmail.com>
2023-09-13 21:45:36 -0400

    test: add specs for has cycle in linked list

--- src/easy/linked_list_cycle.rs
+++ src/easy/linked_list_cycle.rs
@@ -26,3 +26,46 @@ pub fn has_cycle(head: Option<Box<ListNode>>) -> bool {
     }
     false
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_141() {
+        assert_eq!(has_cycle(None), false);
+    }
+
+    #[test]
+    fn test_141_2() {
+        let mut node1 = ListNode::new(1);
+        let mut node2 = ListNode::new(2);
+        let mut node3 = ListNode::new(3);
+        let mut node4 = ListNode::new(4);
+        let node5 = ListNode::new(5);
+
+        node4.next = Some(Box::new(node5));
+        node3.next = Some(Box::new(node4));
+        node2.next = Some(Box::new(node3));
+        node1.next = Some(Box::new(node2));
+
+        assert_eq!(has_cycle(Some(Box::new(node1))), false);
+    }
+
+    #[test]
+    fn test_141_3() {
+        let mut node1 = ListNode::new(1);
+        let mut node2 = ListNode::new(2);
+        let mut node3 = ListNode::new(3);
+        let mut node4 = ListNode::new(4);
+        let mut node5 = ListNode::new(5);
+
+        node1.next = Some(Box::new(node2.clone())); // Clone node2
+        node2.next = Some(Box::new(node3.clone())); // Clone node3
+        node3.next = Some(Box::new(node4.clone())); // Clone node4
+        node4.next = Some(Box::new(node5.clone())); // Clone node5
+        node5.next = Some(Box::new(node1.clone())); // Clone node1
+
+        assert_eq!(has_cycle(Some(Box::new(node1))), false);
+    }
+}
