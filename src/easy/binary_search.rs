0e9b643e206c8d5adcc999c91d4292fda1c403a1
bujosa <davidbujosa@gmail.com>
2023-09-22 22:47:15 +0200

    fix: resolve issue related with runtime error as usize vectors

--- src/easy/binary_search.rs
+++ src/easy/binary_search.rs
@@ -1,13 +1,13 @@
 #![allow(dead_code)]
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {
     let mut left = 0;
-    let mut right = nums.len() - 1;
+    let mut right = nums.len() as i32 - 1;
 
     while left <= right {
         let mid = left + (right - left) / 2;
-        if nums[mid] == target {
+        if nums[mid as usize] == target {
             return mid as i32;
-        } else if nums[mid] < target {
+        } else if nums[mid as usize] < target {
             left = mid + 1;
         } else {
             right = mid - 1;
@@ -32,5 +32,11 @@ mod tests {
     fn test_search() {
         assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
         assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
+    }
+
+    #[test]
+    fn test_search_2() {
+        assert_eq!(search(vec![5], 5), 0);
+        assert_eq!(search(vec![5], -5), -1);
     }
 }
