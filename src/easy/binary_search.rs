f5c01bc189604618c5b3d4a83b6ac59d23419fa1
bujosa <davidbujosa@gmail.com>
2023-09-22 06:54:52 +0200

    test: add spec for binary search

--- src/easy/binary_search.rs
+++ src/easy/binary_search.rs
@@ -1,2 +1,20 @@
 #![allow(dead_code)]
 pub fn search(nums: Vec<i32>, target: i32) -> i32 {}
+
+/*
+    Algorithm:
+        - Binary Search
+    Time: O(logn)
+    Space: O(1)
+*/
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_search() {
+        assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 9), 4);
+        assert_eq!(search(vec![-1, 0, 3, 5, 9, 12], 2), -1);
+    }
+}
