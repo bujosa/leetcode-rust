a8040fe0d5914b36ffd21061eccf5d1d18169664
bujosa <davidbujosa@gmail.com>
2023-09-24 06:03:49 +0200

    test: add specs for search matrix

--- src/medium/search_a_2d_matrix.rs
+++ src/medium/search_a_2d_matrix.rs
@@ -2,3 +2,26 @@
 pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
     true
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_search_matrix() {
+        assert_eq!(
+            search_matrix(
+                vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]],
+                3
+            ),
+            true
+        );
+        assert_eq!(
+            search_matrix(
+                vec![vec![1, 3, 5, 7], vec![10, 11, 16, 20], vec![23, 30, 34, 60]],
+                13
+            ),
+            false
+        );
+    }
+}
