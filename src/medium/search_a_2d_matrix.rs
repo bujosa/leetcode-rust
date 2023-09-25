6da60f4f1892a890550b542529a0e7fcf9303605
bujosa <davidbujosa@gmail.com>
2023-09-25 03:10:10 +0200

    feat: add solution for search a 2d matrix

--- src/medium/search_a_2d_matrix.rs
+++ src/medium/search_a_2d_matrix.rs
@@ -1,7 +1,30 @@
 #![allow(dead_code)]
 pub fn search_matrix(matrix: Vec<Vec<i32>>, target: i32) -> bool {
-    true
+    let m = matrix.len();
+    let n = matrix[0].len();
+    let mut left = 0;
+    let mut right = m * n - 1;
+    while left <= right {
+        let mid = left + (right - left) / 2;
+        let mid_val = matrix[mid / n][mid % n];
+        if mid_val == target {
+            return true;
+        } else if mid_val < target {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    false
 }
+
+/*
+    Approach: Binary Search
+    - Treat the matrix as a sorted array
+    - Use binary search to find the target
+    - Time Complexity: O(log(mn))
+    - Space Complexity: O(1)
+*/
 
 #[cfg(test)]
 mod tests {
