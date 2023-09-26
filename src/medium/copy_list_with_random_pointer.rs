df6f56d85d87b52f9230cd41270ed09b8960bca9
bujosa <davidbujosa@gmail.com>
2023-09-26 05:22:08 +0200

    fix: change box reference to reference counted
    Co-authored-by: Sebastiano Faiella <xeno097.cp@gmail.com>

--- src/medium/copy_list_with_random_pointer.rs
+++ src/medium/copy_list_with_random_pointer.rs
@@ -1,10 +1,11 @@
-use std::collections::HashMap;
+#![allow(dead_code)]
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
 #[derive(Clone)]
 pub struct Node {
     pub val: i32,
-    pub next: Option<Box<Node>>,
-    pub random: Option<Box<Node>>,
+    pub next: Option<Rc<RefCell<Node>>>,
+    pub random: Option<Rc<RefCell<Node>>>,
 }
 
 impl Node {
@@ -18,31 +19,51 @@ impl Node {
     }
 }
 
-pub fn copy_random_list(head: Option<Box<Node>>) -> Option<Box<Node>> {
+pub fn copy_random_list(head: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
     if head.is_none() {
         return None;
     }
 
-    let mut old_new_mapping: HashMap<usize, Box<Node>> = HashMap::new();
-    let mut new_nodes: HashMap<usize, Box<Node>> = HashMap::new();
+    let head = head.unwrap();
 
-    let mut current = head.as_ref();
+    let mut old_new_mapping: HashMap<usize, Rc<RefCell<Node>>> = HashMap::new();
+    let mut new_nodes: HashMap<usize, Rc<RefCell<Node>>> = HashMap::new();
+    let mut current = Some(Rc::clone(&head));
+
     while let Some(node) = current {
-        let id = node.as_ref() as *const Node as usize;
-        let cloned_node = Box::new(Node::new(node.val));
+        let id = node.as_ref() as *const _ as usize;
+
+        let node_value = node.borrow().val;
+        let cloned_node = Rc::new(RefCell::new(Node::new(node_value)));
         old_new_mapping.insert(id, cloned_node);
-        current = node.next.as_ref();
+
+        current = match node.borrow_mut().next {
+            Some(ref next_node) => Some(Rc::clone(next_node)),
+            None => None,
+        };
     }
 
-    // Set the 'next' and 'random' pointers for the cloned nodes.
-    current = head.as_ref();
+    current = Some(Rc::clone(&head));
     while let Some(node) = current {
-        let id = node.as_ref() as *const Node as usize;
-        let cloned_node = old_new_mapping.get_mut(&id).unwrap();
+        let id: usize = node.as_ref() as *const _ as usize;
+        let cloned_node = old_new_mapping.remove(&id).unwrap();
 
-        if let Some(next_node) = &node.next {
-            let id = next_node.as_ref() as *const Node as usize;
-            cloned_node.next = Some({
+        if let Some(next_node) = &node.borrow().next {
+            let id = next_node.as_ref() as *const _ as usize;
+            cloned_node.borrow_mut().next = Some({
+                if let Some(new_node) = new_nodes.get(&id) {
+                    new_node.clone()
+                } else {
+                    let new_node = old_new_mapping.get(&id).unwrap();
+                    new_nodes.insert(id, new_node.clone());
+                    new_node.clone()
+                }
+            });
+        }
+
+        if let Some(random_node) = &node.borrow().random {
+            let id = random_node.as_ref() as *const _ as usize;
+            cloned_node.borrow_mut().random = Some({
                 if let Some(new_node) = new_nodes.get(&id) {
                     new_node.clone()
                 } else {
@@ -53,25 +74,13 @@ pub fn copy_random_list(head: Option<Box<Node>>) -> Option<Box<Node>> {
             });
         }
 
-        if let Some(random_node) = &node.random {
-            let id = random_node.as_ref() as *const Node as usize;
-            cloned_node.random = Some({
-                if let Some(new_node) = new_nodes.get(&id) {
-                    new_node.clone()
-                } else {
-                    let new_node = old_new_mapping.get(&id).unwrap().clone();
-                    new_nodes.insert(id, new_node.clone());
-                    new_node
-                }
-            });
-        }
-        current = node.next.as_ref();
+        old_new_mapping.insert(id, cloned_node);
+
+        current = match node.borrow().next.as_ref() {
+            Some(ref next_node) => Some(Rc::clone(next_node)),
+            None => None,
+        };
     }
 
-    Some(
-        old_new_mapping
-            .get(&(head.as_ref().unwrap().as_ref() as *const Node as usize))
-            .unwrap()
-            .clone(),
-    )
+    old_new_mapping.remove(&(head.as_ref() as *const _ as usize))
 }
