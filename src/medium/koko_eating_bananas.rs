53154433cdc41bed15496406b16a1bcae73a3db7
bujosa <davidbujosa@gmail.com>
2023-09-27 08:41:56 +0200

    feat: add problem and algorithm description for koko eating bananas

--- src/medium/koko_eating_bananas.rs
+++ src/medium/koko_eating_bananas.rs
@@ -1,8 +1,58 @@
 #![allow(dead_code)]
 
 pub fn min_eating_speed(piles: Vec<i32>, h: i32) -> i32 {
-    todo!("Solve this problem")
+    // Find the maximum number of bananas in a pile.
+    let max = piles.iter().max().unwrap(); // O(n)
+
+    // Binary search for the minimum eating speed.
+    // The minimum eating speed is 1.
+    let mut left = 1;
+
+    // The maximum eating speed is the maximum number of bananas in a pile.
+    let mut right = *max;
+
+    while left < right {
+        // The middle eating speed.
+        let mid = left + (right - left) / 2;
+
+        // The number of hours it takes to eat all bananas at the middle eating speed.
+        let mut hours = 0;
+
+        for pile in &piles {
+            // If the number of bananas in the pile is divisible by the middle eating speed,
+            // then the number of hours it takes to eat all bananas in the pile is the number
+            // of bananas in the pile divided by the middle eating speed.
+            if pile % mid == 0 {
+                hours += pile / mid;
+            } else {
+                // Otherwise, the number of hours it takes to eat all bananas in the pile is
+                // the number of bananas in the pile divided by the middle eating speed plus
+                // one.
+                hours += pile / mid + 1;
+            }
+        }
+
+        // If the number of hours it takes to eat all bananas at the middle eating speed is
+        // greater than the number of hours we have, then we need to increase the eating
+        // speed.
+        if hours > h {
+            left = mid + 1;
+        } else {
+            // Otherwise, we need to decrease the eating speed.
+            right = mid;
+        }
+    }
+
+    left
 }
+
+/*
+    Algorithm - Binary Search
+    -------------------------
+
+    Time Complexity: O(n log n)
+    Space Complexity: O(1)
+*/
 
 #[cfg(test)]
 mod tests {
