10f793a097f7dadc3b9649eba04c39620b567100
bujosa <davidbujosa@gmail.com>
2023-09-28 05:50:57 +0200

    feat: add solution for find minimum in rotated sorted array

--- src/medium/find_minimum_in_rotated_sorted_array.rs
+++ src/medium/find_minimum_in_rotated_sorted_array.rs
@@ -1,6 +1,19 @@
 #![allow(dead_code)]
 pub fn find_min(nums: Vec<i32>) -> i32 {
-    0
+    let mut left = 0;
+    let mut right = nums.len() - 1;
+
+    while left < right {
+        let mid = left + (right - left) / 2;
+
+        if nums[mid] > nums[right] {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
 }
 
 #[cfg(test)]
