5a2fe856b3fc25be2f255f9418c628a6720c4006
bujosa <davidbujosa@gmail.com>
2023-09-28 06:14:40 +0200

    feat: add solution for search in rotated sorted array

--- src/medium/search_in_rotated_sorted_array.rs
+++ src/medium/search_in_rotated_sorted_array.rs
@@ -1,5 +1,53 @@
 #![allow(dead_code)]
-pub fn search(nums: Vec<i32>, target: i32) -> i32 {}
+pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+    let mut left = 0;
+    let mut right = nums.len() - 1;
+
+    while left <= right {
+        let mid = left + (right - left) / 2;
+
+        if nums[mid] == target {
+            return mid as i32;
+        }
+
+        if nums[mid] >= nums[left] {
+            if target >= nums[left] && target < nums[mid] {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else {
+            if target <= nums[right] && target > nums[mid] {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    -1
+}
+
+/*
+    Algorithm - Binary Search
+     - Initialize left to 0 and right to the last index of the array
+     - While left <= right
+      - Get the mid index
+      - If the mid value is equal to the target
+       - Return the mid index
+      - If the mid value is greater than or equal to the left value
+       - If the target is greater than or equal to the left value and the target is less than the mid value
+        - Set right to mid - 1
+       - Else
+        - Set left to mid + 1
+      - Else
+       - If the target is greater than or equal to the mid value and the target is less than or equal to the right value
+        - Set left to mid + 1
+       - Else
+        - Set right to mid - 1
+
+     - Return -1
+*/
 
 #[cfg(test)]
 mod tests {
