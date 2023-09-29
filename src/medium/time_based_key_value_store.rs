0a8f2480e02740fdd87cb65f49fe9fcfcd478fbe
bujosa <davidbujosa@gmail.com>
2023-09-29 08:55:43 +0200

    feat: add skeleton for time based key value store

---
+++ src/medium/time_based_key_value_store.rs
@@ -0,0 +1,37 @@
+#![allow(dead_code)]
+struct TimeMap {}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl TimeMap {
+    fn new() -> Self {}
+
+    fn set(&self, key: String, value: String, timestamp: i32) {}
+
+    fn get(&self, key: String, timestamp: i32) -> String {}
+}
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * let obj = TimeMap::new();
+ * obj.set(key, value, timestamp);
+ * let ret_2: String = obj.get(key, timestamp);
+ */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_981() {
+        let obj = TimeMap::new();
+        obj.set("foo".to_string(), "bar".to_string(), 1);
+        assert_eq!(obj.get("foo".to_string(), 1), "bar".to_string());
+        assert_eq!(obj.get("foo".to_string(), 3), "bar".to_string());
+        obj.set("foo".to_string(), "bar2".to_string(), 4);
+        assert_eq!(obj.get("foo".to_string(), 4), "bar2".to_string());
+        assert_eq!(obj.get("foo".to_string(), 5), "bar2".to_string());
+    }
+}
