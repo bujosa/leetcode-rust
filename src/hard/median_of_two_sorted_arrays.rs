27265b3f629871e65ce17e530669419702cb4f29
bujosa <davidbujosa@gmail.com>
2023-10-05 20:31:16 +0200

    feat: add solution for mediam of two sorted arrays

--- src/hard/median_of_two_sorted_arrays.rs
+++ src/hard/median_of_two_sorted_arrays.rs
@@ -1,7 +1,73 @@
 #![allow(dead_code)]
 pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
-    0.000000
+    let (nums1, nums2) = if nums1.len() > nums2.len() {
+        (nums2, nums1)
+    } else {
+        (nums1, nums2)
+    };
+
+    let (m, n) = (nums1.len(), nums2.len());
+    let (mut low, mut high) = (0, m);
+
+    while low <= high {
+        let partition_x = (low + high) / 2;
+        let partition_y = (m + n + 1) / 2 - partition_x;
+
+        let max_x = if partition_x == 0 {
+            i32::MIN
+        } else {
+            nums1[partition_x - 1]
+        };
+
+        let min_x = if partition_x == m {
+            i32::MAX
+        } else {
+            nums1[partition_x]
+        };
+
+        let max_y = if partition_y == 0 {
+            i32::MIN
+        } else {
+            nums2[partition_y - 1]
+        };
+
+        let min_y = if partition_y == n {
+            i32::MAX
+        } else {
+            nums2[partition_y]
+        };
+
+        if max_x <= min_y && max_y <= min_x {
+            return calculate_median(max_x, max_y, min_x, min_y, m, n);
+        } else if max_x > min_y {
+            high = partition_x - 1;
+        } else {
+            low = partition_x + 1;
+        }
+    }
+    0.0
 }
+
+fn calculate_median(max_x: i32, max_y: i32, min_x: i32, min_y: i32, m: usize, n: usize) -> f64 {
+    if (m + n) % 2 == 0 {
+        (max_x.max(max_y) + min_x.min(min_y)) as f64 / 2.0
+    } else {
+        max_x.max(max_y) as f64
+    }
+}
+
+/*
+    Algorithm - Binary Search + two pointers
+        1. Find the middle element of the combined array
+        2. If the combined array is even, find the average of the middle two elements
+        3. If the combined array is odd, return the middle element
+
+    Example:
+    nums1 = [1, 3, 8, 9, 15]
+    nums2 = [7, 11, 18, 19, 21, 25]
+
+    median1 = 8
+*/
 
 #[cfg(test)]
 mod tests {
