69c975664088c23f99c2c8df1e2fe26540646ece
bujosa <davidbujosa@gmail.com>
2023-10-06 19:23:12 +0200

    feat: add main function in reverse nodes in k group

---
+++ src/hard/reverse_nodes_in_k_group.rs
@@ -0,0 +1,17 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
+    todo!()
+}
