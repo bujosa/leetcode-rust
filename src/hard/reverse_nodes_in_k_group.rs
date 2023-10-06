7cf5515ec1ad96e8268cf66cf9f4aa324efa464c
bujosa <davidbujosa@gmail.com>
2023-10-06 19:26:58 +0200

    test(hard): add spec for reverse nodes in k group

--- src/hard/reverse_nodes_in_k_group.rs
+++ src/hard/reverse_nodes_in_k_group.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -10,8 +11,43 @@ impl ListNode {
     fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
+
+    pub fn from_vec(vec: Vec<i32>) -> Option<Box<ListNode>> {
+        let mut head = None;
+        for &val in vec.iter().rev() {
+            let mut node = ListNode::new(val);
+            node.next = head;
+            head = Some(Box::new(node));
+        }
+        head
+    }
 }
 
 pub fn reverse_k_group(head: Option<Box<ListNode>>, k: i32) -> Option<Box<ListNode>> {
     todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_25() {
+        assert_eq!(
+            reverse_k_group(ListNode::from_vec(vec![1, 2, 3, 4, 5]), 2),
+            ListNode::from_vec(vec![2, 1, 4, 3, 5])
+        );
+        assert_eq!(
+            reverse_k_group(ListNode::from_vec(vec![1, 2, 3, 4, 5]), 3),
+            ListNode::from_vec(vec![3, 2, 1, 4, 5])
+        );
+        assert_eq!(
+            reverse_k_group(ListNode::from_vec(vec![1, 2, 3, 4, 5]), 1),
+            ListNode::from_vec(vec![1, 2, 3, 4, 5])
+        );
+        assert_eq!(
+            reverse_k_group(ListNode::from_vec(vec![1]), 1),
+            ListNode::from_vec(vec![1])
+        );
+    }
 }
