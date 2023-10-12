9c0a9478a0c01f6f6d0ece7436978fc9fde48146
bujosa <davidbujosa@gmail.com>
2023-10-12 16:39:55 -0400

    feat: init diameter of binary tree

--- src/easy/mod.rs
+++ src/easy/mod.rs
@@ -7,6 +7,7 @@ pub mod binary_tree_inorder_traversal;
 pub mod climbing_stairs;
 pub mod contains_duplicate;
 pub mod convert_sorted_array_to_binary_search_tree;
+pub mod diameter_of_binary_tree;
 pub mod implement_strstr;
 pub mod invert_binary_tree;
 pub mod length_of_last_word;
