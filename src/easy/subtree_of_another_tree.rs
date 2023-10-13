77302db32c77d65d95a04f3f719a17b715b77fdf
bujosa <davidbujosa@gmail.com>
2023-10-13 19:02:29 -0400

    test(easy): add specs for subtree of another tree

--- src/easy/subtree_of_another_tree.rs
+++ src/easy/subtree_of_another_tree.rs
@@ -25,3 +25,38 @@ impl TreeNode {
 pub fn is_subtree(root: Tree, sub_root: Tree) -> bool {
     todo!()
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_is_not_subtree() {
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+        let sub_root = Some(Rc::new(RefCell::new(TreeNode::new(4))));
+        assert_eq!(is_subtree(root, sub_root), false);
+    }
+
+    #[test]
+    fn test_is_subtree() {
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+        let sub_root = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+        assert_eq!(is_subtree(root, sub_root), true);
+    }
+
+    #[test]
+    fn test_is_subtree_large() {
+        let root = Some(Rc::new(RefCell::new(TreeNode::new(3))));
+        let root_right = Some(Rc::new(RefCell::new(TreeNode::new(4))));
+        let root_left = Some(Rc::new(RefCell::new(TreeNode::new(5))));
+        let root_left_left = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+        let sub_root = Some(Rc::new(RefCell::new(TreeNode::new(5))));
+        let sub_root_left = Some(Rc::new(RefCell::new(TreeNode::new(1))));
+
+        root_left.as_ref().unwrap().borrow_mut().left = root_left_left;
+        root.as_ref().unwrap().borrow_mut().left = root_left;
+        root.as_ref().unwrap().borrow_mut().right = root_right;
+        sub_root.as_ref().unwrap().borrow_mut().left = sub_root_left;
+        assert_eq!(is_subtree(root, sub_root), true);
+    }
+}
