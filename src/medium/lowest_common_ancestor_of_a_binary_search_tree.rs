46d789f148ac7e9862f4f875ddb856db759deb2f
bujosa <davidbujosa@gmail.com>
2023-10-16 18:31:50 -0400

    feat: add squeleton for lowest common ancestor of a binary search tree

---
+++ src/medium/lowest_common_ancestor_of_a_binary_search_tree.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code)]
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type Tree = Option<Rc<RefCell<TreeNode>>>;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Tree,
+    pub right: Tree,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn lowest_common_ancestor(root: Tree, p: Tree, q: Tree) -> Tree {
+    todo!()
+}
