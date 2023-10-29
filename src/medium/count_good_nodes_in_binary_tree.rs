2de98f8cdf837c5adbcc23b979c26c1461562c99
bujosa <davidbujosa@gmail.com>
2023-10-29 19:42:34 -0400

    feat: add good nodes principal function

---
+++ src/medium/count_good_nodes_in_binary_tree.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code)]
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+type Node = Option<Rc<RefCell<TreeNode>>>;
+
+pub fn good_nodes(root: Node) -> i32 {
+    todo!()
+}
