a65e7843ac2b5f4ba7c5b36f82f85e360953bb8d
bujosa <davidbujosa@gmail.com>
2023-10-29 18:44:20 -0400

    feat: add primary function and tree node struct in binary tree right side view

--- src/medium/binary_tree_right_side_view.rs
+++ src/medium/binary_tree_right_side_view.rs
@@ -1,1 +1,27 @@
 #![allow(dead_code)]
+use std::cell::RefCell;
+use std::rc::Rc;
+
+type Node = Option<Rc<RefCell<TreeNode>>>;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Node,
+    pub right: Node,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn right_side_view(root: Node) -> Vec<i32> {
+    todo!()
+}
