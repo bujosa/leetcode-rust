b84bb76bc7496c11707ec02b026968f41b26ec44
bujosa <davidbujosa@gmail.com>
2023-10-29 04:43:53 -0400

    test(medium): add spec for binary tree level order traversal

--- src/medium/binary_tree_level_order_traversal.rs
+++ src/medium/binary_tree_level_order_traversal.rs
@@ -44,3 +44,23 @@ pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
     }
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_level_order() {
+        let mut t1 = TreeNode::new(3);
+        let mut t2 = TreeNode::new(9);
+        let t3 = TreeNode::new(20);
+        let t4 = TreeNode::new(15);
+        let t5 = TreeNode::new(7);
+        t2.left = Some(Rc::new(RefCell::new(t4)));
+        t2.right = Some(Rc::new(RefCell::new(t5)));
+        t1.left = Some(Rc::new(RefCell::new(t2)));
+        t1.right = Some(Rc::new(RefCell::new(t3)));
+        let result = vec![vec![3], vec![9, 20], vec![15, 7]];
+        assert_eq!(level_order(Some(Rc::new(RefCell::new(t1)))), result);
+    }
+}
