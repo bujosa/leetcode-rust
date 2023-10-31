04efde874d276dc5693d1008036f2e556fb92e50
bujosa <davidbujosa@gmail.com>
2023-10-31 03:05:23 -0400

    test(medium): add specs for subsets

--- src/medium/subsets.rs
+++ src/medium/subsets.rs
@@ -2,3 +2,26 @@
 pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
     todo!()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_subsets() {
+        assert_eq!(
+            subsets(vec![1, 2, 3]),
+            vec![
+                vec![3],
+                vec![1],
+                vec![2],
+                vec![1, 2, 3],
+                vec![1, 3],
+                vec![2, 3],
+                vec![1, 2],
+                vec![]
+            ]
+        );
+        assert_eq!(subsets(vec![0]), vec![vec![0], vec![]]);
+    }
+}
