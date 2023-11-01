e23251cd0de9e86a2a7431e4cebdc4d2ce8bccab
bujosa <davidbujosa@gmail.com>
2023-11-01 02:50:45 -0400

    refactor(medium): eliminate result in backtracking function in python just, use the global result, for combination sum ii

--- alternative/medium/combination_sum_ii.py
+++ alternative/medium/combination_sum_ii.py
@@ -2,7 +2,8 @@ from typing import List
 
 def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
     candidates.sort()
-    def backtracking(target, start, current, result):
+    result = []
+    def backtracking(target, start, current):
         if target == 0:
             result.append(list(current))
             return
@@ -12,15 +13,11 @@ def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
             if candidates[i] > target or target - candidates[i] < 0:
                 continue
             current.append(candidates[i])
-            backtracking(target - candidates[i], i + 1, current, result)
+            backtracking(target - candidates[i], i + 1, current)
             current.pop()
             
-
-    
-    result = []
-    backtracking(target, 0, [], result)
+    backtracking(target, 0, [])
     return result
 
 print(combinationSum2([10,1,2,7,6,1,5], 8))
-
-# assert combinationSum2([10,1,2,7,6,1,5], 8) == [[1,1,6],[1,2,5],[1,7],[2,6]]
+assert combinationSum2([10,1,2,7,6,1,5], 8) == [[1,1,6],[1,2,5],[1,7],[2,6]]
