a0f2001a2469bb5426fd0425133f938d9f670671
bujosa <davidbujosa@gmail.com>
2023-10-31 20:09:19 -0400

    feat: add solution for subsets

--- src/medium/subsets.rs
+++ src/medium/subsets.rs
@@ -1,6 +1,14 @@
 #![allow(dead_code)]
 pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    todo!()
+    let mut result = vec![vec![]];
+    for num in nums {
+        let temp = result.clone();
+        for mut item in temp {
+            item.push(num);
+            result.push(item);
+        }
+    }
+    result
 }
 
 #[cfg(test)]
@@ -12,16 +20,15 @@ mod tests {
         assert_eq!(
             subsets(vec![1, 2, 3]),
             vec![
-                vec![3],
+                vec![],
                 vec![1],
                 vec![2],
-                vec![1, 2, 3],
+                vec![1, 2],
+                vec![3],
                 vec![1, 3],
                 vec![2, 3],
-                vec![1, 2],
-                vec![]
+                vec![1, 2, 3]
             ]
         );
-        assert_eq!(subsets(vec![0]), vec![vec![0], vec![]]);
     }
 }
