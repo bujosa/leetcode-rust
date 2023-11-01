970ad994a546bb8b1c180ef7db2014ddc9f9d6e8
bujosa <davidbujosa@gmail.com>
2023-11-01 02:28:22 -0400

    feat: add solution for combination sum 2 in python

---
+++ alternative/medium/combination_sum_ii.py
@@ -0,0 +1,26 @@
+from typing import List
+
+def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
+    candidates.sort()
+    def backtracking(target, start, current, result):
+        if target == 0:
+            result.append(list(current))
+            return
+        for i in range (start, len(candidates)):
+            if i > start and candidates[i] == candidates[i - 1]:
+                continue
+            if candidates[i] > target or target - candidates[i] < 0:
+                continue
+            current.append(candidates[i])
+            backtracking(target - candidates[i], i + 1, current, result)
+            current.pop()
+            
+
+    
+    result = []
+    backtracking(target, 0, [], result)
+    return result
+
+print(combinationSum2([10,1,2,7,6,1,5], 8))
+
+# assert combinationSum2([10,1,2,7,6,1,5], 8) == [[1,1,6],[1,2,5],[1,7],[2,6]]
