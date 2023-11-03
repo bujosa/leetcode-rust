67b1f33a1fdf3427376463670ca9757addafce5f
bujosa <davidbujosa@gmail.com>
2023-11-02 21:16:09 -0400

    feat: add mock kth largest

---
+++ src/easy/kth_largest_element_in_a_stream.rs
@@ -0,0 +1,17 @@
+#![allow(dead_code)]
+struct KthLargest {}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * let obj = KthLargest::new(k, nums);
+ * let ret_1: i32 = obj.add(val);
+ */
+impl KthLargest {
+    fn new(k: i32, nums: Vec<i32>) -> Self {
+        KthLargest {}
+    }
+
+    fn add(&self, val: i32) -> i32 {
+        todo!()
+    }
+}
