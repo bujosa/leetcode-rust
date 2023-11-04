a5a7ebba9d4245303a09f2fd9bec1d70ac6d31c2
bujosa <davidbujosa@gmail.com>
2023-11-04 04:24:24 -0400

    feat: add solution for delete and earn

---
+++ alternative/medium/delete_and_earn.py
@@ -0,0 +1,31 @@
+from typing import List
+
+# This is a solution for a problem frequently asked in Amazon coding interviews.
+def deleteAndEarn(nums: List[int]) -> int:
+    hash_map = {}
+    for num in nums:
+        if num in hash_map:
+            hash_map[num] += 1
+        else:
+            hash_map[num] = 1
+    
+    nums = sorted(list(set(nums)))
+    earn1, earn2 = 0, 0
+
+    for i in range(len(nums)):
+        curEarn = nums[i] * hash_map[nums[i]]
+
+        if i > 0 and nums[i] - nums[i - 1] == 1:
+            temp = earn2
+            earn2 = max(earn1 + curEarn, earn2)
+            earn1 = temp
+        else:
+            temp = earn2
+            earn2 = curEarn + earn2
+            earn1 = temp
+    
+    return earn2
+
+
+assert deleteAndEarn([3, 4, 2]) == 6
+assert deleteAndEarn([2, 2, 3, 3, 3, 4]) == 9
