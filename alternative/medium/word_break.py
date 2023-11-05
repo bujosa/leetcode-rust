f76176fa011579185c7b81ea3f1d66ba28ddb59f
bujosa <davidbujosa@gmail.com>
2023-11-04 20:08:11 -0400

    feat: init word break in py

---
+++ alternative/medium/word_break.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def wordBreak(s: str, wordDict: List[str]) -> bool:
+    True
+
+assert wordBreak("leetcode", ["leet", "code"]) == True
+assert wordBreak("applepenapple", ["apple", "pen"]) == True
+assert wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]) == False        
