733c16db7dd854f8fcbd4eae41ffe614b5fc69fb
bujosa <davidbujosa@gmail.com>
2023-11-05 04:33:59 -0400

    feat: add solution for rotting oranges using bfs

---
+++ alternative/medium/rotting_oranges.py
@@ -0,0 +1,33 @@
+from collections import deque
+from typing import List
+
+def orangesRotting(grid: List[List[int]]) -> int:
+    ROWS, COLS = len(grid), len(grid[0])
+    fresh, time = 0, 0
+    q = deque()
+    for r in range(ROWS):
+        for c in range(COLS):
+            if grid[r][c] == 1:
+                fresh+=1
+            if grid[r][c] == 2:
+                q.append([r, c])
+
+    directions = [[0, 1], [0, -1], [1, 0], [-1,0]]
+    while q and fresh > 0:
+        qlen = len(q)
+        for _ in range(qlen):
+            r, c = q.popleft()
+
+            for dr, dc in directions:
+                row, col = dr + r, dc + c
+                if (row in range(ROWS) and col in range(COLS) and grid[row][col] == 1):
+                    grid[row][col] = 2
+                    q.append([row,col])
+                    fresh -= 1
+                
+        time += 1
+    
+    return time if fresh == 0 else -1
+
+assert orangesRotting([[2,1,1],[1,1,0],[0,1,1]]) == 4
+assert orangesRotting([[2,1,1],[0,1,1],[1,0,1]]) == -1
