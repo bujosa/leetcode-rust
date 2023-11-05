6efc269503c6bc84ef92aafd3147748536f03ee9
bujosa <davidbujosa@gmail.com>
2023-11-05 01:31:14 -0400

    feat: add solution for clone graph in py

---
+++ alternative/medium/clone_graph.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+from typing import Optional
+
+def cloneGraph(node: Optional['Node']) -> Optional['Node']:
+    if not node:
+        return None
+
+    visited = {}
+
+    def dfs(node):
+        if node in visited:
+            return visited[node]
+        clone = Node(node.val, [])
+        visited[node] = clone
+        for n in node.neighbors:
+            clone.neighbors.append(dfs(n))
+        return clone
+
+    return dfs(node)
+
+
+print(cloneGraph([[2,4],[1,3],[2,4],[1,3]]))
