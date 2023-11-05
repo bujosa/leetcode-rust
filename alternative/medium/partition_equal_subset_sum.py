29247203e918077b4d13cca7daf07e908249c958
bujosa <davidbujosa@gmail.com>
2023-11-04 22:15:49 -0400

    feat: add solution for 416 in python

--- alternative/medium/partition_equal_subset_sum.py
+++ alternative/medium/partition_equal_subset_sum.py
@@ -1,9 +1,19 @@
 from typing import List
 
 def canPartition(nums: List[int]) -> bool:
-    True
+    total = sum(nums)
+    if total % 2 != 0:
+        return False
+    target = total // 2
+    dp = [False] * (target + 1)
+    dp[0] = True
+    for num in nums:
+        for i in range(target, num - 1, -1):
+            dp[i] = dp[i] or dp[i - num]
+    return dp[-1]
 
 assert canPartition([1,5,11,5]) == True
 assert canPartition([1,2,3,5]) == False
+assert canPartition([1,2]) == False
 assert canPartition([1,2,3,4,5,6,7]) == True
-assert canPartition([1,2,3,4,5,6,7,8]) == False
+assert canPartition([1,2,3,4,5,6,7,8]) == True
