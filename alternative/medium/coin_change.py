e3268ab5da09bc3da0e8ed90f29e2d7004e88cd5
bujosa <davidbujosa@gmail.com>
2023-11-04 21:54:44 -0400

    style: fix documentation with """

--- alternative/medium/coin_change.py
+++ alternative/medium/coin_change.py
@@ -11,22 +11,24 @@ def coinChange(coins: List[int], amount: int) -> int:
     
     return -1 if dp[amount] > amount else dp[amount]
 
-# Algorithm is based on the following recurrence relation:
-# dp[a] = min(dp[a], dp[a - c] + 1)
-# where:
-#   a is the amount of money
-#   c is the coin value
-#   dp[a] is the minimum number of coins needed to make up the amount a
-#   dp[a - c] is the minimum number of coins needed to make up the amount a - c
-#   + 1 is the coin we are adding to the solution
+"""
+Algorithm is based on the following recurrence relation:
+dp[a] = min(dp[a], dp[a - c] + 1)
+where:
+  a is the amount of money
+  c is the coin value
+  dp[a] is the minimum number of coins needed to make up the amount a
+  dp[a - c] is the minimum number of coins needed to make up the amount a - c
+  + 1 is the coin we are adding to the solution
 
-# The algorithm is based on the following observations:
-#   The minimum number of coins needed to make up the amount a is the minimum
-#      number of coins needed to make up the amount a - c plus one coin.
-# 
-# Analysis:
-# Time complexity: O(a * c) = o(n^2)
-# Space complexity: O(a) = O(n)
+The algorithm is based on the following observations:
+  The minimum number of coins needed to make up the amount a is the minimum
+     number of coins needed to make up the amount a - c plus one coin.
+
+Analysis:
+Time complexity: O(a * c) = o(n^2)
+Space complexity: O(a) = O(n)
+"""
 
 assert coinChange([1,2,5], 11) == 3
 assert coinChange([2], 3) == -1
