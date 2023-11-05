3819fc0879a6cb53cd3548219991ab75daaba13c
bujosa <davidbujosa@gmail.com>
2023-11-05 04:35:07 -0400

    docs: add algorithm description for rotting oranges

--- alternative/medium/rotting_oranges.py
+++ alternative/medium/rotting_oranges.py
@@ -1,6 +1,7 @@
 from collections import deque
 from typing import List
 
+# Amazon Problem
 def orangesRotting(grid: List[List[int]]) -> int:
     ROWS, COLS = len(grid), len(grid[0])
     fresh, time = 0, 0
@@ -28,6 +29,20 @@ def orangesRotting(grid: List[List[int]]) -> int:
         time += 1
     
     return time if fresh == 0 else -1
+
+"""
+    Algorithm - Breadth First Search
+    Time Complexity - O(n)
+    Space Complexity - O(n)
+
+    Explanation:
+    1. We first check the number of fresh oranges and add the rotten oranges to a queue.
+    2. We then iterate through the queue and check the adjacent cells for fresh oranges.
+    3. If there are fresh oranges, we add them to the queue and decrement the fresh orange count.
+    4. We repeat this process until the queue is empty or there are no fresh oranges left.
+    5. If there are no fresh oranges left, we return the time taken to rot all the oranges.
+    6. Else, we return -1.
+"""
 
 assert orangesRotting([[2,1,1],[1,1,0],[0,1,1]]) == 4
 assert orangesRotting([[2,1,1],[0,1,1],[1,0,1]]) == -1
