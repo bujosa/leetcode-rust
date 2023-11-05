70ed1d199c8c90e6817aee130b72980a4b9f2730
bujosa <davidbujosa@gmail.com>
2023-11-04 21:54:33 -0400

    feat: init partition equal subset sum

---
+++ alternative/medium/partition_equal_subset_sum.py
@@ -0,0 +1,9 @@
+from typing import List
+
+def canPartition(nums: List[int]) -> bool:
+    True
+
+assert canPartition([1,5,11,5]) == True
+assert canPartition([1,2,3,5]) == False
+assert canPartition([1,2,3,4,5,6,7]) == True
+assert canPartition([1,2,3,4,5,6,7,8]) == False
