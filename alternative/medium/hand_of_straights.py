8cb1dd2c32dc34fdd82a8da190f2cf02b3a822c2
bujosa <davidbujosa@gmail.com>
2023-11-15 18:45:48 -0400

    test(medium): add specs for is N Straight Hand

---
+++ alternative/medium/hand_of_straights.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def isNStraightHand(hand: List[int], groupSize: int) -> bool:
+  True
+
+assert isNStraightHand([1,2,3,6,2,3,4,7,8], 3) == True
+assert isNStraightHand([1,2,3,4,5], 4) == False
+assert isNStraightHand([1,2,3,4,5], 5) == True
